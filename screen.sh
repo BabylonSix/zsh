a5b38ff71a0fb0ea1765ca7f43dc10346de4e1ea
Von Bravo <babylon.six@me.com>
2016-06-08 09:16:35 -0700

    better screen variable declarations

--- screen.sh
+++ screen.sh
@@ -1,7 +1,11 @@
 screenSize() {
 
+# Declare variables
+export screenWidth
+export screenHeight
+
 # automatically get screen width
-export screenWidth=`osascript <<EOF
+screenWidth=`osascript <<EOF
 tell app "Finder"
 	-- get screen dimensions
 	set dimensions to bounds of window of desktop
@@ -11,7 +15,7 @@ end tell
 EOF`
 
 # automatically get screen height
-export screenHeight=`osascript <<EOF
+screenHeight=`osascript <<EOF
 tell app "Finder"
 	-- get screen dimensions
 	set dimensions to bounds of window of desktop
@@ -22,20 +26,20 @@ EOF`
 
 # resulting screen sizes:
 # screenHeight
-h1_3=$((screenHeight / 3))
-h2_3=$((screenHeight / 3 * 2))
+export h1_3=$((screenHeight / 3))
+export h2_3=$((screenHeight / 3 * 2))
 
-h1_4=$((screenHeight / 4))
-h2_4=$((screenHeight / 4 * 2))
-h3_4=$((screenHeight / 4 * 3))
+export h1_4=$((screenHeight / 4))
+export h2_4=$((screenHeight / 4 * 2))
+export h3_4=$((screenHeight / 4 * 3))
 
 # screenWidth
-w1_3=$((screenWidth / 3))
-w2_3=$((screenWidth / 3 * 2))
+export w1_3=$((screenWidth / 3))
+export w2_3=$((screenWidth / 3 * 2))
 
-w1_4=$((screenWidth / 4))
-w2_4=$((screenWidth / 4 * 2))
-w3_4=$((screenWidth / 4 * 3))
+export w1_4=$((screenWidth / 4))
+export w2_4=$((screenWidth / 4 * 2))
+export w3_4=$((screenWidth / 4 * 3))
 
 }; screenSize # invoke screenSize fucntion
 
