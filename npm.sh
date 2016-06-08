f941a0ee74e492b72ea5e35942f53397ed5ba117
Von Bravo <babylon.six@me.com>
2016-06-07 18:19:45 -0700

    added script to automatically install desired NPM packages
    for this script to work, the node version set through
    ‘nvm alias default <<version>>’ has to include the full version name
    so v6.1.0 or v6.0.0 instead of v6.1 or v6, respectively

--- npm.sh
+++ npm.sh
@@ -25,3 +25,61 @@ npml() { npm ls --depth=0 "$@" 2>/dev/null; }
 
 # Lists Global NPM Packages
 npmg() { npm ls --depth=0 -g 2>/dev/null; }
+
+
+
+# Default NPM Libraries
+npmPackages=(
+  babel-cli
+  babel-eslint
+  babel-preset-es2015
+  browser-sync
+  browserify
+  cheerio
+  css2stylus
+  csslint
+  eslint
+  express
+  express-generator
+  graceful-fs
+  gulp
+  html2jade
+  htmlhint
+  iconv-lite
+  jade
+  jade-lint
+  jstransformer
+  jstransformer-babel
+  jstransformer-coffee-script
+  jstransformer-marked
+  jstransformer-stylus
+  osascript
+  postcss
+  postcss-cli
+  sass-lint
+  simplecrawler
+  standard
+  standard-format
+  stylint
+  stylus
+  trash
+  vtop
+  webpack
+  xmlbuilder
+)
+
+for package in $npmPackages
+do
+  # Enter the full node version number on `nvm alias default` for command tool to work properly
+  currentNode=~/.nvm/versions/node/$(cat ~/.nvm/alias/default)/lib/node_modules
+  # check if package is installed
+  if [[ -a $currentNode/$package ]]; then
+  # if program is installed - do nothing
+    'true'
+  else
+    # install package globally
+    npm install -g $package
+    # divide the installs visually with 2 newlines
+    print '\n'
+  fi
+done
