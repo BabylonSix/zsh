15a163e0813efb78c6d179b0762d6dcea5a17851
Von Bravo <babylon.six@me.com>
2016-06-08 18:53:06 -0700

    change: manpath variable constructor is a function

--- path.sh
+++ path.sh
@@ -30,3 +30,24 @@ setupPATH() {
 path() { $SHELL -lc 'echo $PATH | tr : "\n"'; } # display path variable
 
 ts() { $SHELL -lc 'echo hello'; } # use this guy for debugging the path file
+
+
+
+setupMANPATH() {
+  manpathDirs=(
+    /usr/local/opt/gnu-sed/libexec/gnuman
+    /usr/local/opt/gnu-tar/libexec/gnuman
+    /usr/local/opt/findutils/libexec/gnubin
+    /usr/local/opt/coreutils/libexec/gnubin
+    $MANPATH
+  )
+
+  for dir in $manpathDirs
+  do
+    MANPATH+=":$dir" # add our own directories to the system $PATH variable
+  done
+}; setupMANPATH
+
+
+
+manpath() { $SHELL -lc 'echo $MANPATH | tr : "\n"'; } # display manpath variable
