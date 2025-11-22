ad4ab61ad04988f2048bd0b4b5b2ead707aab6dd
Von Bravo <BabylonSix@users.noreply.github.com>
2025-11-22 04:30:14 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 # big sur update
 #
 # Directory structure of zsh setup file should look as following
-# ~/bin
+# ~/.dotfiles
 # ├── web
 # └── zsh
 #     └── startup.sh
@@ -10,28 +10,31 @@
 # If the directory structure looks like the above,
 # Run the following command
 #
-# ln -sf ~/bin/zsh/startup.sh ~/.zshrc; zsh -l; setupzsh
+# ln -sf ~/.dotfiles/zsh/startup.sh ~/.zshrc; zsh -l; setupzsh
 
 
 # Sources
-. ~/bin/zsh/setup.sh                 # setup zsh functions
-. ~/bin/zsh/path.sh                  # load $PATH first
-. ~/bin/zsh/package-managers.sh      # load package managers
-. ~/bin/zsh/colors.sh                # Set color variables
-. ~/bin/zsh/shell.sh                 # Look & Feel of the shell
-. ~/bin/zsh/directory-tools.sh       # Directory creation|navigation
-. ~/bin/zsh/directories.sh
-. ~/bin/zsh/tools.sh                 # Tools & Utilities
-. ~/bin/zsh/node-setup.sh            # NVM & NPM Setup + Packages
-. ~/bin/zsh/node-tools.sh            # NPM completions, etc...
-. ~/bin/zsh/completion.sh            # ZSH completions
-. ~/bin/zsh/ssh.sh                   # SSH configuration
-. ~/bin/zsh/git.sh                   # Git configuration
-. ~/bin/zsh/extract.sh               # unzip utility
-. ~/bin/zsh/alias.sh                 # Shortcut/Alias commands
-. ~/bin/zsh/screen.sh                # screensize tools
-. ~/bin/web/web-path.sh              # web project creation tools
-. `brew --prefix`/etc/profile.d/z.sh # Lets Z work
-. ~/bin/nvim/setup.sh                # neovim setup
-. /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-. ~/bin/zsh/test-tools.sh            # use for testing stuff out
+. ~/.dotfiles/zsh/setup.sh                 # setup zsh functions
+. ~/.dotfiles/zsh/path.sh                  # load $PATH first
+. ~/.dotfiles/zsh/package-managers.sh      # load package managers
+. ~/.dotfiles/zsh/colors.sh                # Set color variables
+. ~/.dotfiles/zsh/shell.sh                 # Look & Feel of the shell
+. ~/.dotfiles/zsh/directory-tools.sh       # Directory creation|navigation
+. ~/.dotfiles/zsh/directories.sh
+. ~/.dotfiles/zsh/tools.sh                 # Tools & Utilities
+. ~/.dotfiles/zsh/node-setup.sh            # NVM & NPM Setup + Packages
+. ~/.dotfiles/zsh/node-tools.sh            # NPM completions, etc...
+. ~/.dotfiles/zsh/completion.sh            # ZSH completions
+. ~/.dotfiles/zsh/ssh.sh                   # SSH configuration
+. ~/.dotfiles/zsh/git.sh                   # Git configuration
+. ~/.dotfiles/zsh/extract.sh               # unzip utility
+. ~/.dotfiles/zsh/alias.sh                 # Shortcut/Alias commands
+. ~/.dotfiles/zsh/screen.sh                # screensize tools
+. ~/.dotfiles/web/web-path.sh              # web project creation tools
+. ~/.dotfiles/nvim/setup.sh                # neovim setup
+. ~/.dotfiles/zsh/test-tools.sh            # use for testing stuff out
+
+test -e /Users/bravo/.iterm2_shell_integration.zsh && source /Users/bravo/.iterm2_shell_integration.zsh || true
+
+# Added by LM Studio CLI (lms)
+export PATH="$PATH:/Users/bravo/.lmstudio/bin"
