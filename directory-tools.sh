177d9f1eae92d5e4efa9be8cbc4027f2677b5303
Von Bravo <BabylonSix@users.noreply.github.com>
2025-11-22 04:24:54 -0800

    Update directory-tools.sh
    add yazi function

--- directory-tools.sh
+++ directory-tools.sh
@@ -121,3 +121,13 @@ u9() { cd ../../../../../../../../.. ||exit; l; }
 sd() {
   pwd | sed -E "s/\/Users\/$USER/~/g"
 }
+
+
+# launches yazi and upon close sets cd to last visited directory
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
