7c1ed5cae210b4b0a2e2d9c7dfffed07b217b122
Von Bravo <BabylonSix@users.noreply.github.com>
2025-11-21 22:34:37 -0800

    Update alias.sh
    change bin to .dotfiles

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 
 # DotFile Settings
-alias reload='source ~/bin/zsh/setup.sh'
+alias reload='source ~/.dotfiles/zsh/setup.sh'
 alias r='reload; clear'
 
 # System Settings
@@ -9,24 +9,24 @@ alias eshells='ot /etc/shells'
 alias elint='ot ~/.eslintrc' # configure eslint
 
 # Edit Config Files
-alias ea='ot ~/bin/zsh/alias.sh && reload'
-alias etools='ot ~/bin/zsh/tools.sh && reload'
+alias ea='ot ~/.dotfiles/zsh/alias.sh && reload'
+alias etools='ot ~/.dotfiles/zsh/tools.sh && reload'
 alias et='etools'
-alias escreen='ot ~/bin/zsh/screen.sh'
-alias epath='ot ~/bin/zsh/path.sh' # edit $PATH
-alias eshell='ot ~/bin/zsh/shell.sh'
-alias ecolor='ot ~/bin/zsh/colors.sh'
-alias edir='ot ~/bin/zsh/directory-tools.sh'
-alias edirs='ot ~/bin/zsh/directories.sh'
-alias enpm='ot ~/bin/zsh/node-setup.sh'
-alias essh='ot ~/bin/zsh/ssh.sh'
-alias esetup='ot ~/bin/zsh/setup.sh'
-alias ebin='ot ~/bin/'
-alias egit='ot ~/bin/zsh/git.sh'
-alias eweb='ot ~/bin/web/'
-alias ezsh='ot ~/bin/zsh/'
-alias esj='ot ~/bin/web/sj/'
-alias evim='ot ~/bin/nvim/settings.vim'
+alias escreen='ot ~/.dotfiles/zsh/screen.sh'
+alias epath='ot ~/.dotfiles/zsh/path.sh' # edit $PATH
+alias eshell='ot ~/.dotfiles/zsh/shell.sh'
+alias ecolor='ot ~/.dotfiles/zsh/colors.sh'
+alias edir='ot ~/.dotfiles/zsh/directory-tools.sh'
+alias edirs='ot ~/.dotfiles/zsh/directories.sh'
+alias enpm='ot ~/.dotfiles/zsh/node-setup.sh'
+alias essh='ot ~/.dotfiles/zsh/ssh.sh'
+alias esetup='ot ~/.dotfiles/zsh/setup.sh'
+alias ebin='ot ~/.dotfiles/'
+alias egit='ot ~/.dotfiles/zsh/git.sh'
+alias eweb='ot ~/.dotfiles/web/'
+alias ezsh='ot ~/.dotfiles/zsh/'
+alias esj='ot ~/.dotfiles/web/sj/'
+alias evim='ot ~/.dotfiles/nvim/settings.vim'
 
 # Directory Shortcuts
 alias hd='cd ~/Desktop; l'
@@ -35,11 +35,11 @@ alias hc='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/My\ Files/Clients; 
 alias he='cd ~/Experiments; l'
 alias hp='cd ~/Personal\ Projects; l'
 alias hpd='cd ~/Personal\ Projects/Daily\ Project; l'
-alias hweb='cd ~/bin/web; l'
+alias hweb='cd ~/.dotfiles/web; l'
 alias hwp='cd ~/Web\ Projects; l'
 alias hw='hwp'
 alias hvp='cd ~/Video\ Projects; l'
 alias hdbx='cd ~/Library/CloudStorage/Dropbox; o'
-alias hzsh='cd ~/bin/zsh; l'
-alias hvim='cd ~/bin/nvim; l'
+alias hzsh='cd ~/.dotfiles/zsh; l'
+alias hvim='cd ~/.dotfiles/nvim; l'
 alias hbrew='cd /opt/homebrew/Cellar; l'
