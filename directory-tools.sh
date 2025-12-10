fd9a9ead89a9d3ee354ad1eface17632bd744749
Von Bravo <BabylonSix@users.noreply.github.com>
2025-12-10 03:23:29 -0800

    converted all directory-tools to functions

--- directory-tools.sh
+++ directory-tools.sh
@@ -1,7 +1,7 @@
 # File and Directory creation
-alias f='touch'
-alias d='mkdir -pv'
-alias lns='ln -s'
+f() { touch "$@"; }
+d() { mkdir -pv "$@"; }
+lns() { ln -s "$@"; }
 
 md() {
   if [[ $# = 0 ]]; then
@@ -14,9 +14,9 @@ md() {
 
 
 # Open files | directories
-alias o='open .'
-alias of='ot .'
-alias od='ot .'
+o() { open .; }
+of() { ot .; }
+od() { ot .; }
 
 
 # Take ownership of directory
@@ -44,41 +44,44 @@ v() {
 
 
 # Safe deletion
-alias srm='trash'
+srm() { trash "$@"; }
 
 
 # Zip directory or file
-zipr() { zip -r "$1.zip" "$1"; }
+zipr() {
+  [[ -z "$1" ]] && { print "\n${RED}ERROR:${NC} zipr <file-or-dir>"; return 1; }
+  zip -r "$1.zip" "$1"
+}
 
 
 # Directory display (use eza if available, fallback to ls)
 if command -v eza &>/dev/null; then
-  alias l='clear; eza'
-  alias ll='clear; eza -lh'
-  alias la='clear; eza -a'
-  alias lla='clear; eza -alh'
-  alias lh='clear; eza -d .[^.]*'
-  alias llh='clear; eza -dlh .[^.]*'
-  alias lm='clear; eza -s modified'
-  alias llm='clear; eza -lhs modified'
+  l()   { clear; eza "$@"; }
+  la()  { clear; eza -a "$@"; }
+  ll()  { clear; eza -lh "$@"; }
+  lla() { clear; eza -alh "$@"; }
+  lh()  { clear; eza -d .[^.]* "$@"; }
+  llh() { clear; eza -dlh .[^.]* "$@"; }
+  lm()  { clear; eza -s modified "$@"; }
+  llm() { clear; eza -lhs modified "$@"; }
 else
-  alias ls='ls -G'
-  alias l='clear; ls'
-  alias ll='clear; ls -lh'
-  alias la='clear; ls -A'
-  alias lla='clear; ls -Alh'
-  alias lh='clear; ls -d .[^.]*'
-  alias llh='clear; ls -dlh .[^.]*'
-  alias lm='clear; ls -t'
-  alias llm='clear; ls -lht'
+  ls()  { command ls -G "$@"; }
+  l()   { clear; ls "$@"; }
+  ll()  { clear; ls -lh "$@"; }
+  la()  { clear; ls -A "$@"; }
+  lla() { clear; ls -Alh "$@"; }
+  lh()  { clear; ls -d .[^.]* "$@"; }
+  llh() { clear; ls -dlh .[^.]* "$@"; }
+  lm()  { clear; ls -t "$@"; }
+  llm() { clear; ls -lht "$@"; }
 fi
 
 
 # Directory tree
-alias tree='tree -C'
-alias t='tree -I "node_modules"'
+tree() { eza --tree --color=always -I ".git|node_modules" "$@"; }
+t() { tree "$@"; }
 
-tl() { tree -L "$@" -I "node_modules"; }
+tl() { tree -L "$@"; }
 
 t2() { tl 2; }
 t3() { tl 3; }
@@ -89,15 +92,13 @@ t7() { tl 7; }
 t8() { tl 8; }
 t9() { tl 9; }
 
-alias tll='tree -p'
-alias tg='tree -g'
-alias to='tree -u'
-alias ta='tree -a -I ".git|node_modules"'
-alias tlla='tree -ap -I ".git|node_modules"'
-
+tll() { tree -l "$@"; }
+tlla() { tll -a "$@"; }
+tllg() { tll --git-repos --no-user --no-time --no-filesize "$@"; }
+tllga() { tllg -a "$@"; }
 
 # Directory navigation
-alias b='cd -; l'
+b() { cd -; l; }
 
 u() { cd .. || return 1; l; }
 u2() { cd ../.. || return 1; l; }
