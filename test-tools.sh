c4bdce35a29935ee57146bdbbcc340e48edc0144
Von Bravo <BabylonSix@users.noreply.github.com>
2025-12-18 17:44:41 -0800

    cleaned up test-tools.sh

--- test-tools.sh
+++ test-tools.sh
@@ -61,65 +61,7 @@ rnmr() {
 }
 
 
-########################################################################
-# Script Name: list_files.zsh
-# Description: A Zsh script for directory listing with clipboard support.
-# 
-# Usage: Execute with flags (e.g., ./list_files.zsh --cd)
-# Environment: macOS with Zsh
-#
-########################################################################
-#
-# CURRENT CAPABILITIES
-# --------------------
-#   --cd Flag:
-#     - Lists all files and directories in the current directory.
-#     - Formats directories with a trailing slash.
-#     - Copies the formatted list to the macOS clipboard.
-#
-# EXPANDABILITY
-# -------------
-#   - Structured with a 'case' statement for easy addition of new flags.
-#   - Each flag can trigger specific commands for extended functionality.
-#
-# USAGE NOTES
-# -----------
-#   - Run the script with desired flags to execute corresponding actions.
-#   - Example: './list_files.zsh --cd' lists contents of the current directory.
-#
-# ENVIRONMENT
-# -----------
-#   - Designed for Zsh in a macOS environment.
-#   - Utilizes 'pbcopy' for clipboard interaction.
-#
-########################################################################
-
-copy() {
-  for arg in "$@"; do
-    case "$arg" in
-      --cd)
-        for file in *(D); do
-          if [[ -d $file ]]; then
-            echo "$file/"
-          else
-            echo "$file"
-          fi
-        done | pbcopy
-        ;;
-      
-      # Template for additional flags
-      # --other-flag)
-      #   # Implement other flag's functionality
-      #   ;;
-
-      *)
-        echo "Invalid flag: $arg"
-        ;;
-    esac
-  done
-}
-
-split_txt() {
+txtsplit() {
     # Check for the -c flag for case sensitivity
     local case_sensitive=0
     if [[ $1 == "-c" ]]; then
@@ -139,35 +81,6 @@ split_txt() {
         return 1
     fi
 
-    # Process the file using awk
-    awk -v delimiter="$delimiter" -v case_sensitive="$case_sensitive" '
-    BEGIN {IGNORECASE=(case_sensitive==0)}
-    {
-        if ((IGNORECASE && tolower($0) ~ tolower(delimiter)) || (!IGNORECASE && $0 ~ delimiter)) {
-            in_block = 0
-            if (output_file != "") {
-                close(output_file)
-            }
-            output_file = ""
-            next
-        }
 
-        if (in_block == 0 && $0 !~ /^[[:space:]]*$/) {
-            gsub(/[\/]/, "_", $0)
-            output_file = $0 ".txt"
-            in_block = 1
-        }
-
-        if (in_block == 1 && output_file != "") {
-            print $0 > output_file
-        }
-    }
-    END {
-        if (output_file != "") {
-            close(output_file)
-        }
-    }' "$file"
-}
-
-# Usage: split_txt "text-delimiter" "file-to-be-split.txt"
-# With -c flag for case sensitivity: split_txt -c "Text-Delimiter" "file-to-be-split.txt"
+# Usage: txtsplit "text-delimiter" "file-to-be-split.txt"
+# With -c flag for case sensitivity: txtsplit -c "Text-Delimiter" "file-to-be-split.txt"
